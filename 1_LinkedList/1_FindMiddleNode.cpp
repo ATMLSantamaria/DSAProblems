ed117665cfe717aab7b5d8f41e14187a28389c31
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-11-21 13:50:49 +0100

    Optimal Solution for finding the middle node in a linked list

--- 1_LinkedList/1_FindMiddleNode.cpp
+++ 1_LinkedList/1_FindMiddleNode.cpp
@@ -2,8 +2,6 @@
 #include <cassert>
 using namespace std;
 
-//Complexity T O(N) S (N + N*8bytes) Vector of pointers
-
 // This is an input struct. Do not edit.
 class LinkedList {
  public:
@@ -13,8 +11,34 @@ class LinkedList {
   LinkedList(int value) { this->value = value; }
 };
 
-void TraverseAndStore(LinkedList * iterator, std::vector<LinkedList*> & storage);
+LinkedList * TraverseWithTwoPointers(LinkedList * linkedList) {
+  LinkedList * slow_iterator_ = linkedList;
+  LinkedList * fast_iterator_ = linkedList;
+  
+  int control_slow_pointer_movement_ = 0;  
+  while (fast_iterator_ != nullptr) {
+    fast_iterator_ = fast_iterator_->next;
+    
+    if (control_slow_pointer_movement_ == 1) {
+      slow_iterator_ = slow_iterator_->next;
+    }
+    //NOTA: Bitwise XOR operation to make a switch from 1 to 0 to 1 to 0
+    control_slow_pointer_movement_ = control_slow_pointer_movement_ ^ 1;
+  }
+  return slow_iterator_;	
+}
 
+//Iterate with a fast pointer and a slow motion pointer
+//Complexity T O(N) S (1) Vector of pointers
+LinkedList* OptimalMiddleMiddleNode(LinkedList* linkedList) {
+   //First return the case of only one node
+   if (linkedList->next == nullptr) return linkedList;
+   LinkedList * slow_it = TraverseWithTwoPointers(linkedList);
+   return slow_it;
+}
+
+void TraverseAndStore(LinkedList * iterator, std::vector<LinkedList*> & storage);
+//Complexity T O(N) S (N + N*8bytes) Vector of pointers
 LinkedList* MiddleNodeStorage(LinkedList* linkedList) {
   // Write your code here.
   std::vector<LinkedList*> storage_;
@@ -31,7 +55,6 @@ LinkedList* MiddleNodeStorage(LinkedList* linkedList) {
   // Even if 1, index = 0
   
   index = storage_.size() / 2;
-    
   return storage_[index];
 }
 
@@ -42,24 +65,21 @@ void TraverseAndStore(LinkedList * iterator, std::vector<LinkedList*> & storage)
   }
 }
 
+
+//Complexity T O(N+n/2) S (1) Vector of pointers
 int TraverseAndCount(LinkedList * iterator);
 LinkedList* MiddleNodeTwoIterations(LinkedList* linkedList) {
   // Write your code here.
-
   //Create Iterator
   LinkedList* iterator = linkedList;
   //Count
   int size = TraverseAndCount(iterator);
-
   //reset iterator to the beginning
   //iterator = linkedList;
-
   if (size == 1) return linkedList;
-
   int index = size / 2;
   //If size 3 -> index = 1
   //If size 4 -> index = 2
-
   for (int i = 0;i < index;++i){
     iterator = iterator->next; 
   }
@@ -109,6 +129,12 @@ int main() {
 
   result = MiddleNodeTwoIterations(linkedList);
   assert(result == fourNode);
+
+  result = nullptr;
+
+  result = OptimalMiddleMiddleNode(linkedList);
+  assert(result == fourNode);
+  
   delete linkedList;
   delete twoNode;
   delete threeNode;

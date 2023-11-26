8d3ec03fbc9b02a9b3e38e2102aabbdf2732ea65
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-11-26 23:45:07 +0100

    Comparison of the Brute Force and the real Insertion Sort

--- 4_SortingAlgorithm/1_InsertionSort.cpp
+++ 4_SortingAlgorithm/1_InsertionSort.cpp
@@ -2,24 +2,52 @@
 #include <iostream>
 using namespace std;
 
+//Brute Force Sort. Take T O(n^2) even in best case
 void InsertionSort(vector<int> & array) {
   // Write your code here.
   int to_swap = 0;
-  int index_of_ecurrently_analysed_element =0;
-  for (int i = 0; i < array.size();++i){
-    index_of_ecurrently_analysed_element = i;
-    for (int j = i; j >= 0; --j) {
+  int index = 0;
 
+  for (int i = 0; i < array.size();++i){
+    index = i;
+    for (int j = i; j >= 0; --j) {
       //When move the element to the left, we want to follow the element, so we need to update
       //it index
-      if (array[index_of_ecurrently_analysed_element] < array[j]) {
+      if (array[index] < array[j]) {
         to_swap = array[j];
-        array[j] = array[index_of_ecurrently_analysed_element];
-        array[index_of_ecurrently_analysed_element] =to_swap;
+        array[j] = array[index];
+        array[index] =to_swap;
 
-        index_of_ecurrently_analysed_element = j;
+        index = j;
       }
     }
+  }
+  //return array;
+}
+
+//This is real insertion sort. The other is a bastard version that perform extra calculations, since it does not take into consideration that at each spep
+// the list to the left is sorted.
+
+// Complexity Best case T O(n) Average and Worst O(n^2). S O(1)
+void InsertionSortReal(vector<int> & array) {
+  // Write your code here.
+  int to_swap = 0;
+  int index = 0;
+  
+  for (int i = 1; i < array.size();++i){
+    index = i;
+
+      while(array[index] < array[index - 1] && index != 0) {
+
+        to_swap = array[index];
+
+        array[index] = array[index - 1];
+
+        array[index -1] =to_swap;
+
+        index--;
+      }
+
   }
   //return array;
 }
@@ -28,7 +56,7 @@ int main() {
 
   std::vector<int> target = {1,6,55,4,2,133,4,2,5,55,6,88,4,3,-1000,-100,8765};
 
-  InsertionSort(target);
+  InsertionSortReal(target);
 
   for (auto & i : target) {
     std::cout << i << ",";

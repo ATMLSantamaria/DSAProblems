49741e744be5f4bde9d83d00cfeec2f845b5e2b5
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-11-26 18:27:28 +0100

    Iterative Fibonacci

--- 5_Recursion/0_Fibonacci.cpp
+++ 5_Recursion/0_Fibonacci.cpp
@@ -16,6 +16,36 @@ int GetNthFibRecursive(int n) {
   return GetNthFibRecursive(n-1) + GetNthFibRecursive(n-2);
 }
 
+// T O(n) S O(1)
+int GetNthFibIterative(int n) {
+  // Write your code here.
+  if (n == 1) {
+    return 0;
+  }
+  if (n == 2) {
+    return 1;
+  }
+  int fib_n_2 = 0;
+  int fib_n_1 = 1;
+  int res = fib_n_2 + fib_n_1;
+
+
+  //Compute F(n-1) and F(n-2)
+  for (int i = 3; i <= n;++i) {
+    res = fib_n_2 + fib_n_1;
+   
+    fib_n_2 = fib_n_1;
+    fib_n_1 = res;
+  }
+  return res;
+}
+
+
 int main() {
   assert(GetNthFibRecursive(7) == 8);
+  assert(GetNthFibIterative(7) == 8);
+
+  assert(GetNthFibIterative(100000000) == GetNthFibIterative(100000000)); //here start to slow
+
+
 }

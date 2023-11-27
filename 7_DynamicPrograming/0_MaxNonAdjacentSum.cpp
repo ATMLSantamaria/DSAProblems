dc21a43fe65a9937b4ced6ad531105f5d308141f
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-11-27 21:45:51 +0100

    Solution without extra space

--- 7_DynamicPrograming/0_MaxNonAdjacentSum.cpp
+++ 7_DynamicPrograming/0_MaxNonAdjacentSum.cpp
@@ -8,7 +8,7 @@ using namespace std;
 //  maxSum[i] = max(maxSum[i-1],maxSum[i-2]+array[i])
 
 //Solution in T O(N) and S O(N) where N is the number of elements in the array
-int maxSubsetSumNoAdjacent(vector<int> array) {
+int MaxSubsetSumNoAdjacent(vector<int> array) {
 
   if (array.empty()) {
     return 0;
@@ -32,7 +32,36 @@ int maxSubsetSumNoAdjacent(vector<int> array) {
   return res.back();
 }
 
+int MaxSubsetSumNoAdjacentWithoutExtraSpace(vector<int> array) {
+
+  if (array.empty()) {
+    return 0;
+  }
+  if ( array.size() == 1) {
+    return array[0];
+  }
+  
+  int maxSum = 0;
+  int two_before = array[0];
+  int one_before = two_before > array[1] ? two_before : array[1];
+
+  if (array.size() == 2) {
+    return array[0] > array[1] ? array[0] : array[1];
+  }
+
+  for (int i = 2; i < array.size();++i) {
+    maxSum = std::max(one_before,two_before + array[i]);
+    two_before = one_before;
+    one_before = maxSum;
+    
+  }
+  
+  return maxSum;
+}
+
 int main() {
   std::vector<int> target = {75,105,120,75,90,135};
+  assert(MaxSubsetSumNoAdjacent(target) == 330);
+  assert(MaxSubsetSumNoAdjacentWithoutExtraSpace(target) == MaxSubsetSumNoAdjacent(target));
 
 }

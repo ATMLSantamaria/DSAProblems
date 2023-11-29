c245f1d803939bf57b3aa57574e496bbcb1458af
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-11-29 18:01:38 +0100

    Create TreeLike, InsertIn TreeLike and Print TreeLike Template functions

--- 8_BinarySearchTree/z_Auxiliar.h
+++ 8_BinarySearchTree/z_Auxiliar.h
@@ -9,12 +9,64 @@
 #include <queue>
 
 
+template<typename TreeType>
+void InsertIn(int value,TreeType *root);
+
+
 template <typename TreeType>
-TreeType * createTree() {
-  BinaryTreeTypeTree* root = new TreeType(10);
+TreeType * createTreeAux() {
+  TreeType* root = new TreeType(10);
   int elements[] = {5, 15, 3, 7, 13, 17, 2, 4, 6, 8, 12, 14, 16, 18, 1, 9, 11, 19, 20};
   for (int element : elements) {
-    root->insert(element);
+    InsertIn(element,root);
   }
   return root;
+}
+
+template<typename TreeType>
+void InsertIn(int value,TreeType *root) {
+    TreeType* current = root;
+    while (true) {
+      if (value < current->value) {
+        if (current->left == nullptr) {
+          current->left = new TreeType(value);
+          break;
+        } else {
+          current = current->left;
+        }
+      } else {
+        if (current->right == nullptr) {
+          current->right = new TreeType(value);
+          break;
+        } else {
+          current = current->right;
+        }
+      }
+    }
+}
+
+template<typename TreeType>
+void printLevelOrder(TreeType* root) {
+  if (root == nullptr) return;
+
+  std::queue<TreeType*> q;
+  q.push(root);
+
+  while (!q.empty()) {
+    int levelSize = q.size();
+
+    for (int i = 0; i < levelSize; i++) {
+      TreeType* current = q.front();
+      q.pop();
+      std::cout << current->value << " ";
+
+      if (current->left != nullptr) {
+        q.push(current->left);
+      }
+      if (current->right != nullptr) {
+        q.push(current->right);
+      }
+    }
+    std::cout << std::endl;
+  }
 }

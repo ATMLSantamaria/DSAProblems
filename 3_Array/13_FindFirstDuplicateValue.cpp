50ab45544cded01e1397327dd60b16130a2d3fc9
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-12-02 14:46:59 +0100

    Solution to find first duplicate with hash table

---
+++ 3_Array/13_FindFirstDuplicateValue.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+//Solution with HASH TABLE -> C
+//Should perform O(n) worst case, but with ints the hash function should handle it
+
+int firstDuplicateValue(vector<int> array) {
+  std::unordered_map<int,int> tracking;
+  for (auto & i : array) {
+
+    tracking[i] = trac
+  }
+  
+  return -1; }

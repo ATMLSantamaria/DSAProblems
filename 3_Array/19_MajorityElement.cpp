6360a713a64980376a8874298dc3d61a42ed8b31
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2023-12-12 22:42:16 +0100

    Brute force algorithm for finding majority element

--- 3_Array/19_MajorityElement.cpp
+++ 3_Array/19_MajorityElement.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-
+// T O(n)
 int majorityElement(vector<int> array) {
   // Write your code here.
   int appearences = 0;
@@ -23,6 +23,24 @@ int majorityElement(vector<int> array) {
   }     
  
   return temptative;
+}
+
+//Brute force approach in O(n2)
+int majorityElement(vector<int> array) {
+  // Write your code here.
+  int appearences = 0;
+  for (int i = 0;i < array.size();++i) {
+    appearences = 0;
+    for (int j = i;j < array.size();++j) {
+      if (array[i] == array[j]){
+        appearences++;
+      }
+    }
+    if (appearences > (array.size() / 2 )) {
+      return array[i];
+    }
+  }
+  return -1;
 }
 
 

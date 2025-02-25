b6d8c0b65773d02128e964680be9d30455512f08
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2025-02-25 22:33:15 +0100

    first solution without using properties of BST

---
+++ 2_BinaryTree/3_ClosestBinarySearchTreeValue.py
@@ -0,0 +1,41 @@
+import heapq
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        # Store minimum value
+        heap = []
+        
+        # traverseTree(root,target,heap)
+
+        cleverTraverseTree(root,target,heap)
+
+        return heapq.heappop(heap)[1]
+
+
+# First approach. Simply to solve the problem checking all nodes if the tree
+    
+    # Create tuples of (diff,value) and put them in a min heap
+
+    # Go across all the tree
+
+    # when finished pop the minimum diff from the heap.
+            # Check if there are more diff with same value
+
+    # This approach is going to take O(n) since we will check all the nodes in the tree
+
+def traverseTree(node,target,heap):
+    # Base case
+    if node is None:
+        return
+
+    # Recursive call adn logic
+    dif = abs(target - node.val)
+    heapq.heappush(heap,(dif,node.val))
+
+    traverseTree(node.left,target,heap)
+    traverseTree(node.right,target,heap)

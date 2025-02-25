f6537ef137ceada1348e3cc53df44ab7520ab9ea
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2025-02-25 18:41:56 +0100

    short function to print a binary tree in DFS

---
+++ 2_BinaryTree/0_BranchSums.py
@@ -0,0 +1,78 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+
+
+def printNodeDSF(node):
+    if node is None:
+        return
+    if node.left is None and node.right is None:
+        print(node.value)
+    printNodeDSF(node.left)
+    printNodeDSF(node.right)
+
+    # print(node.value)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root = BinaryTree(1)
+B = BinaryTree(2)
+# C = BinaryTree(2)
+D = BinaryTree(4)
+E = BinaryTree(5)
+
+F = BinaryTree(8)
+G = BinaryTree(9)
+H = BinaryTree(10)
+
+I = BinaryTree(3)
+J = BinaryTree(6)
+K = BinaryTree(7)
+
+
+
+root.left = B 
+root.right = I
+
+B.left = D
+B.right = E
+
+D.left = F
+D.right = G
+
+E.left = H
+
+I.left = J
+I.right = K
+
+printNodeDSF(root)
